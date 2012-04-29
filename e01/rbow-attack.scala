e06035af2bc02cce9c4248ff120e5bc3e593753a
covin <covin@gmx.net>
2012-04-29 03:37:39 +0200

    [e01] rbow-table script yields a valid password

--- e01/rbow-attack.scala
+++ e01/rbow-attack.scala
@@ -6,13 +6,49 @@ val FILE = "rainbow.tbl"
 abstract class Rainbow {
     self: HashAndReg =>
 
-    val table: Map[Hash, Key]
+    val table: Map[Hash, Seq[Key]]
 
-    def lookup(hash: Hash): Key = {
-        // TODO implement logic here
+    def walk (from: Key, to: Hash, limit: Hash): Option[Key] = {
+      var _k = from
 
-        r(hash)
+    println("walk: %s" format from)
+      h(from) match {
+        case hash if hash == to => Some(from)
+        case hash if hash == limit => None
+        case hash => walk( r(hash), to, limit)
+      }
     }
+
+    def lookup(pwhash: Hash): Option[Key] = {
+      var _hash = pwhash
+      var _tbl  = table
+      var _key: Option[Key] = None
+
+      while (_key == None && !_tbl.isEmpty) {
+        println("new chain lookup")
+
+        _tbl.get(_hash) match {
+          case None => _hash = h(r(_hash))
+          case Some(keys) => {
+            println("hash match: %s" format _hash)
+            val p = keys
+            .map(walk(_, pwhash, _hash))
+            .filter(_ != None)
+
+            if (p isEmpty) {
+                // update map: remove chains
+                println("[INFO] dismiss chain(s): %s" format _hash)
+                _tbl -= _hash
+            } else {
+              // we have finished
+              _key = p.head
+            }
+          }
+        }
+      }
+      _key
+    }
+
 }
 
 
@@ -43,30 +79,34 @@ object CharRules {
        (i%26) + a toChar
     }
 
-    implicit def str2seqInt (s: Seq[Char]): Seq[Int] = {
-        s.map(char2int)
-    }
-
     implicit def seqInt2str(c: Seq[Int]): Seq[Char] = {
         c.map(int2char)
     }
 }
 
 trait FooBar extends HashAndReg {
+    import CharRules._
 
-    type Hash = Seq[Int]
-    type Key  = Seq[Int]
+    type Hash = Seq[Char]
+    type Key  = Seq[Char]
 
     val b = 1
 
     def h(key: Key) : Hash = {
-        key
+        val hash: Hash = key
         .sliding(2, 2)
-        .map(_.foldLeft(0)(_+_)) toSeq
+        .map(_.foldLeft(0)(_+_-a))
+        .map(_%26).toSeq
+        println("h(key): %s -> %s" format (key.mkString, hash.mkString))
+        hash
     }
 
     def r(hash: Hash) : Key = {
-        hash ++ (hash.reverse.map(_+b))
+        val part1 = hash
+        val part2: Seq[Char] = hash.reverse.map(_+b-a).map(_%26)
+        val key: Key = part1 ++ part2
+        println("r(hash): %s -> %s" format (hash.mkString, key.mkString))
+        key
     }
 }
 
@@ -74,26 +114,34 @@ trait FooBar extends HashAndReg {
  *
  */
 object Run {
-    import CharRules._
 
-    def loadMap: Map[Seq[Int], Seq[Int]] = io.Source.fromFile(FILE).getLines().map { (l:String) =>
-        val p = l.splitAt(8)
-        val h: Seq[Int] = p._2.tail.toSeq
-        val k: Seq[Int] = p._1.toSeq
-        (h -> k)
-    } toMap
+    type A = Seq[Char]
+
+    val file_map: Map[A, Seq[A]] = {
+      val lmap = io.Source.fromFile(FILE)
+        .getLines()
+        .map {(l:String) =>
+          val p = l.splitAt(8)
+          val h = p._2.tail.toSeq // remove leading whitespace
+          val k = p._1.toSeq
+          (h -> k)
+        }
+      lmap.toSeq
+      .groupBy (_._1)
+      .map { case (k,v) => (k -> v.map(_._2)) } toMap
+    }
 
     object rainbow extends Rainbow with FooBar {
-        override val table = loadMap
+      override val table = file_map
     }
 
     def apply(s: String) = {
-        val k: Seq[Char] = rainbow.lookup(s.toSeq)
-        k
+        rainbow.lookup(s.toSeq)
     }
 
 }
 
-val pass: Seq[Char] = Run("xbdz")
-
-println("retrieved pass word: %s" format pass)
+Run("xbdz") match {
+  case None => println("no password found")
+  case Some(p) => println("retrieved pass word: %s" format p)
+}
